13091922b55d1219165b96f92ee483905f05ae40
Liam Bao <zhiv.bao@gmail.com>
2023-08-21 08:25:23 -0400

    Proxylab-Part1: Implementing a sequential web proxy

--- proxylab/proxy.c
+++ proxylab/proxy.c
@@ -1,14 +1,193 @@
-#include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+typedef struct {
+    char host[MAXLINE];
+    char port[MAXLINE];
+    char path[MAXLINE];
+} uri_t;
 
-int main()
-{
-    printf("%s", user_agent_hdr);
-    return 0;
+void doit(int fd);
+
+void parse_uri(char *uri, uri_t *uri_info);
+
+void build_requesthdrs(rio_t *rio_client, uri_t *uri_info, char *requesthdrs);
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+
+void sigpipe_handler(int sig);
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    Signal(SIGPIPE, sigpipe_handler);
+    listenfd = Open_listenfd(argv[1]);
+
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);
+        Close(connfd);
+    }
+}
+
+/*
+ * doit - handle one HTTP request/response transaction
+ */
+void doit(int fd) {
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char requesthdrs[MAXLINE];
+    rio_t rio_client, rio_server;
+    uri_t *uri_info;
+    int serverfd;
+    size_t n;
+
+    /* Read request line and headers */
+    Rio_readinitb(&rio_client, fd);
+    if (!Rio_readlineb(&rio_client, buf, MAXLINE)) return;
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version);
+    if (strcasecmp(method, "GET")) {
+        clienterror(fd, method, "501", "Not Implemented",
+                    "Tiny does not implement this method");
+        return;
+    }
+
+    uri_info = (uri_t *) Calloc(1, sizeof(uri_t));
+    parse_uri(uri, uri_info);
+    build_requesthdrs(&rio_client, uri_info, requesthdrs);
+
+    /* Connect to server */
+    serverfd = Open_clientfd(uri_info->host, uri_info->port);
+    free(uri_info);
+    Rio_readinitb(&rio_server, serverfd);
+    Rio_writen(serverfd, requesthdrs, strlen(requesthdrs));
+
+    while ((n = Rio_readlineb(&rio_server, buf, MAXLINE)) != 0) {
+        printf("proxy received %d bytes, sending to client\n", (int) n);
+        Rio_writen(fd, buf, n);
+    }
+    Close(serverfd);
+}
+
+/*
+ * parse_uri - parse URI to get host, port, and path
+ */
+void parse_uri(char *uri, uri_t *uri_info) {
+    static const char *port_http = "80";
+    char *host_ptr;
+    char *port_ptr;
+    char *path_ptr;
+
+    if ((host_ptr = strstr(uri, "//")) != NULL) {
+        if ((port_ptr = strstr(host_ptr + 2, ":")) != NULL) {
+            int tmp;
+            sscanf(port_ptr + 1, "%d%s", &tmp, uri_info->path);
+            sprintf(uri_info->port, "%d", tmp);
+            *port_ptr = '\0';
+        } else {
+            if ((path_ptr = strstr(host_ptr + 2, "/")) != NULL) {
+                strcpy(uri_info->port, port_http);
+                strcpy(uri_info->path, path_ptr);
+                *path_ptr = '\0';
+            }
+        }
+        strcpy(uri_info->host, host_ptr + 2);
+    } else {
+        strcpy(uri_info->port, port_http);
+        path_ptr = strstr(uri, "/");
+        strcpy(uri_info->path, path_ptr != NULL ? path_ptr : "/");
+    }
+}
+
+/*
+ * build_requesthdrs - build HTTP request headers
+ */
+void build_requesthdrs(rio_t *rio_client, uri_t *uri_info, char *requesthdrs) {
+    static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+    static const char *conn_hdr = "Connection: close\r\n";
+    static const char *proxy_conn_hdr = "Proxy-Connection: close\r\n";
+    static const char *req_hdr_fmt = "GET %s HTTP/1.0\r\n";
+    static const char *host_hdr_fmt = "Host: %s\r\n";
+    static const char *eof_hdr = "\r\n";
+    static const char *host_key = "Host";
+    static const char *conn_key = "Connection";
+    static const char *proxy_conn_key = "Proxy-Connection";
+    static const char *user_agent_key = "User-Agent";
+
+    char buf[MAXLINE], req_hdr[MAXLINE], host_hdr[MAXLINE], other_hdrs[MAXLINE];
+    sprintf(req_hdr, req_hdr_fmt, uri_info->path);
+    /* Parse headers from client */
+    while (Rio_readlineb(rio_client, buf, MAXLINE) > 0) {
+        if (!strcmp(buf, eof_hdr)) break;
+
+        if (!strncasecmp(buf, host_key, strlen(host_key))) {
+            strcpy(host_hdr, buf);
+            continue;
+        }
+
+        if (strncasecmp(buf, conn_key, strlen(conn_key)) &&
+            strncasecmp(buf, proxy_conn_key, strlen(proxy_conn_key)) &&
+            strncasecmp(buf, user_agent_key, strlen(user_agent_key))) {
+            strcat(other_hdrs, buf);
+        }
+
+        if (strlen(host_hdr) == 0) {
+            sprintf(host_hdr, host_hdr_fmt, uri_info->host);
+        }
+
+        sprintf(requesthdrs,
+                "%s%s%s%s%s%s%s",
+                req_hdr,
+                host_hdr,
+                conn_hdr,
+                proxy_conn_hdr,
+                user_agent_hdr,
+                other_hdrs,
+                eof_hdr);
+    }
+}
+
+/*
+ * clienterror - returns an error message to the client
+ */
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
+    char buf[MAXLINE];
+
+    /* Print the HTTP response headers */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* Print the HTTP response body */
+    sprintf(buf, "<html><title>Tiny Error</title>");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<body bgcolor=""ffffff"">\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "%s: %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<p>%s: %s\r\n", longmsg, cause);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<hr><em>The Tiny Web server</em>\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+}
+
+void sigpipe_handler(int sig) {
+    printf("SIGPIPE handled\n");
+    return;
 }
